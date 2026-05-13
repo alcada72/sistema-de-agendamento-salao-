8fd9ae416cd8ca3aaf45fde6399ee615176e3197
alcada72 <alcada300@gmail.com>
2026-05-13 21:53:34 +0100

    fix ts errors v2

--- bacckend/src/index.ts
+++ bacckend/src/index.ts
@@ -1,5 +1,5 @@
 import cors from "cors";
-import express from "express";
+import express, { Response } from "express";
 import helmet from "helmet";
 import http from "http";
 import path from "path";
@@ -7,49 +7,51 @@ import { mainRouter } from "./router/main.routes";
 import { requestInterceptor } from "./utils/requestInterceptor";
 
 const app = express();
-const httpServer = http.createServer(app)
+const httpServer = http.createServer(app);
 
 const corsOptions = {
   origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization",]
+  allowedHeaders: ["Content-Type", "Authorization"]
 };
 
 app.use(cors(corsOptions));
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static("public"));
 
-// Servir uploads
+/**
+ * Uploads públicos
+ */
 app.use(
   "/uploads",
-  express.static(path.join(__dirname, "../uploads"), {
-    setHeaders: (res) => {
+  express.static(path.resolve("uploads"), {
+    setHeaders: (res: Response) => {
       res.setHeader("Access-Control-Allow-Origin", "*");
       res.setHeader("Cross-Origin-Resource-Policy", "cross-origin");
-    },
+    }
   })
 );
 
 app.use(requestInterceptor);
 
-
-//Configuração de rotas
+// Rotas
 app.use(mainRouter);
 
 const PORT = process.env.PORT || 8080;
 const BASE_URL = process.env.BASE_URL || `http://localhost:${PORT}`;
 
 httpServer.listen(PORT, () => {
-  console.log(` 
+  console.log(`
   ╔════════════════════════════════════════╗
-  ║    JMC-cachiungo -  v1.0.0             ║
-  ║  JMC-CACHIUNGO agendamento de serviços ║
+  ║    JMC-cachiungo - v1.0.0              ║
+  ║  Agendamento de serviços               ║
   ╠════════════════════════════════════════╣
-  ║  🚀 Servidor rodando na porta ${PORT}  ║
-  ║  🌐 URL Base: ${BASE_URL}             ║  
-  ║  📦 Ambiente: ${process.env.NODE_ENV || 'development'}║
+  ║  🚀 Porta: ${PORT}
+  ║  🌐 URL: ${BASE_URL}
+  ║  📦 Ambiente: ${process.env.NODE_ENV || "development"}
   ╚════════════════════════════════════════╝
   `);
 });
